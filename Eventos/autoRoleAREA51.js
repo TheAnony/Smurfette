b4918a45d9a43883ad830fe77930583d9bba334f
The Anony <theanonimooo14@gmail.com>
2023-09-02 15:26:24 -0300

    Verificação se o membro é bot

--- Eventos/autoRoleAREA51.js
+++ Eventos/autoRoleAREA51.js
@@ -5,7 +5,7 @@ const client = require('../index')
 client.on("guildMemberAdd", (member) => {
     let cargo_autorole = member.guild.roles.cache.get("934896199666905158") // Coloque o ID do cargo
     if (!cargo_autorole) return console.log("❌ | O AUTOROLE não está configurado.");
-    if(member.guild.id !== '921107172316815410') return;
+    if(member.guild.id !== '921107172316815410' && member.bot) return;
   
     member.roles.add(cargo_autorole.id).catch(err => {
       console.log(`❌ Não foi possível adicionar o cargo de autorole no usuário ${member.user.tag}.`)
