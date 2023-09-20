8ddaa9928e84080f368b392a5b0d134c79fcc7b0
The Anony <theanonimooo14@gmail.com>
2023-09-20 10:11:50 -0300

    Melhoria no regex para pegar tempo em ms

--- funções.js
+++ funções.js
@@ -1,107 +1,110 @@
 const ms = require('ms');
 
 function pegarDataNow() {
-    const date = new Date();
-    const dia = date.getDate() < 10 ? '0' + date.getDate() : date.getDate()
-    const mes = date.getMonth() + 1 < 10 ? `0${date.getMonth() + 1}` : date.getMonth()
-    const ano = date.getFullYear();
-    const hora = date.getHours();
-    const minuto = date.getMinutes();
+        const date = new Date();
+        const dia = date.getDate() < 10 ? '0' + date.getDate() : date.getDate()
+        const mes = date.getMonth() + 1 < 10 ? `0${date.getMonth() + 1}` : date.getMonth()
+        const ano = date.getFullYear();
+        const hora = date.getHours();
+        const minuto = date.getMinutes();
 
-    return `${dia}/${mes}/${ano}, às ${hora}h${minuto}min.`
+        return `${dia}/${mes}/${ano}, às ${hora}h${minuto}min.`
 }
 
 function convertTimeStringToList(timeString) {
-    // Regular expression pattern to match time units.
-    var pattern = /(\d+)([a-z]+)/g;
-    // Find all matches of the pattern in the time string.
-    var matches = timeString.match(pattern);
-    // Convert the matches to a list of time units.
-    var timeUnits = [];
-    for (let i = 0; i < matches.length; i++) {
-            // Split each match into the quantity and the unit.
-            var match = matches[i].match(/(\d+)([a-z]+)/);
-            timeUnits.push(match[1]);  // Quantity
-            timeUnits.push(match[2]);  // Unit
-    }
-    return timeUnits;
+        var pattern = /(\d+)([a-z]+)/g;
+        var matches = timeString.match(pattern)
+        var timeUnits = [];
+        for (let i = 0; i < matches.length; i++) {
+                var match = matches[i].match(/(\d+)([a-z]+)/);
+                timeUnits.push(match[1]);  // Quantity
+                timeUnits.push(match[2]);  // Unit
+        }
+        return timeUnits;
 }
 
 function StringToMilliseonds(timeString) {
-    // Define the milliseconds equivalents for each time unit
-    const millisecondsInYear = 31_536_000_000;
-    const millisecondsInMonth = 2_628_000_000;
-    const millisecondsInDay = 86_400_000;
-    const millisecondsInHour = 3_600_000;
-    const millisecondsInMinute = 60_000;
-    const millisecondsInSecond = 1_000;
-    let timeUnits = timeString.toLowerCase().split(' ');
+        const millisecondsInYear = 31_536_000_000;
+        const millisecondsInMonth = 2_628_000_000;
+        const millisecondsInDay = 86_400_000;
+        const millisecondsInHour = 3_600_000;
+        const millisecondsInMinute = 60_000;
+        const millisecondsInSecond = 1_000;
+        let timeUnits = timeString.toLowerCase().split(/ +/g)
+        const regex = /^(?=.*[a-zA-Z])(?=.*\d).+$/;
+        let ElementsFilter = [];
 
-    const regex = /[a-zA-Z]/;
-    const str = timeUnits[0]
-    const resultado = regex.test(str)
-    if (resultado === true) timeUnits = convertTimeStringToList(timeString)
+        timeUnits.forEach(item => {
+                let resul = regex.test(item)
+                if (resul) {
+                        timeUnits.push(convertTimeStringToList(item))
+                        ElementsFilter.push(item)
+                }
+        });
+        timeUnits = timeUnits.flat();
+        timeUnits = timeUnits.filter(item => !ElementsFilter.includes(item))
 
-    let totalMilliseconds = 0;
+        let totalMilliseconds = 0;
 
-    for (let i = 0; i < timeUnits.length; i += 2) {
-            const unit = timeUnits[i + 1];
-            switch (unit) {
-                    case 'years':
-                    case 'anos':
-                    case 'year':
-                    case 'ano':
-                    case 'y':
-                            totalMilliseconds += parseInt(timeUnits[i]) * millisecondsInYear;
-                            break;
-                    case 'months':
-                    case 'meses':
-                    case 'month':
-                    case 'mês':
-                    case 'm':
-                            totalMilliseconds += parseInt(timeUnits[i]) * millisecondsInMonth;
-                            break;
-                    case 'days':
-                    case 'dias':
-                    case 'day':
-                    case 'dia':
-                    case 'd':
-                            totalMilliseconds += parseInt(timeUnits[i]) * millisecondsInDay;
-                            break;
-                    case 'hours':
-                    case 'horas':
-                    case 'hour':
-                    case 'hrs':
-                    case 'hora':
-                    case 'hr':
-                    case 'h':
-                            totalMilliseconds += parseInt(timeUnits[i]) * millisecondsInHour;
-                            break;
-                    case 'minutes':
-                    case 'minutos':
-                    case 'minute':
-                    case 'minuto':
-                    case 'min':
-                    case 'mnt':
-                            totalMilliseconds += parseInt(timeUnits[i]) * millisecondsInMinute;
-                            break;
-                    case 'seconds':
-                    case 'segundos':
-                    case 'sgs':
-                    case 'second':
-                    case 'segundo':
-                    case 's':
-                    case 'sg':
-                            totalMilliseconds += parseInt(timeUnits[i]) * millisecondsInSecond;
-                            break;
-                    default:
-            }
-    }
-    return totalMilliseconds;
+        for (let i = 0; i < timeUnits.length; i += 2) {
+                const unit = timeUnits[i + 1];
+                switch (unit) {
+                        case 'years':
+                        case 'anos':
+                        case 'year':
+                        case 'ano':
+                        case 'y':
+                                totalMilliseconds += parseInt(timeUnits[i]) * millisecondsInYear;
+                                break;
+                        case 'months':
+                        case 'meses':
+                        case 'month':
+                        case 'mês':
+                        case 'm':
+                                totalMilliseconds += parseInt(timeUnits[i]) * millisecondsInMonth;
+                                break;
+                        case 'days':
+                        case 'dias':
+                        case 'day':
+                        case 'dia':
+                        case 'd':
+                                totalMilliseconds += parseInt(timeUnits[i]) * millisecondsInDay;
+                                break;
+                        case 'hours':
+                        case 'horas':
+                        case 'hour':
+                        case 'hrs':
+                        case 'hora':
+                        case 'hr':
+                        case 'h':
+                                totalMilliseconds += parseInt(timeUnits[i]) * millisecondsInHour;
+                                break;
+                        case 'minutes':
+                        case 'minutos':
+                        case 'minute':
+                        case 'minuto':
+                        case 'min':
+                        case 'mnt':
+                                totalMilliseconds += parseInt(timeUnits[i]) * millisecondsInMinute;
+                                break;
+                        case 'seconds':
+                        case 'segundos':
+                        case 'sgs':
+                        case 'second':
+                        case 'segundo':
+                        case 's':
+                        case 'sg':
+                                totalMilliseconds += parseInt(timeUnits[i]) * millisecondsInSecond;
+                                break;
+                        default:
+                         totalMilliseconds = 'err'
+                }
+        }
+        return totalMilliseconds;
 }
 
 function millisecondsToString(timeMil) {
-    
+
 }
 
 module.exports = { pegarDataNow, StringToMilliseonds };
