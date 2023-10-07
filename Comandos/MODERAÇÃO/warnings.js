e0f93b23a71797d18bfabed8dba64f6a68b269f1
The Anony <theanonimooo14@gmail.com>
2023-10-06 23:28:41 -0300

    Comando de ver os logs de warns feito

---
+++ Comandos/MODERAÇÃO/warnings.js
@@ -0,0 +1,45 @@
+const { EmbedBuilder, ApplicationCommandType, ApplicationCommandOptionType, User } = require("discord.js")
+const emojis = require('../../emojis.json')
+const { pegarDataNow } = require('../../funcoes');
+const warningSchema = require('../../Models/SistemaDeWarns');
+
+module.exports = {
+    name: "warnings", // Coloque o nome do comando
+    description: "『 MODERAÇÃO 』Veja a auditoria de avisos do membro.", // Coloque a descrição do comando
+    type: ApplicationCommandType.ChatInput,
+    options: [
+        {
+            name: 'membro',
+            description: `Escolha o membro para visualizar os logs dele.`,
+            type: ApplicationCommandOptionType.User,
+            required: true,
+        },
+    ],
+
+    run: async (client, interaction) => {
+        const membro = interaction.options.getUser('membro');
+        const warnDate = pegarDataNow();
+
+        await warningSchema.findOne({ GuildID: interaction.guild.id, UserID: membro.id, Username: membro.username }).then(async(data) => {
+            if(data) {
+                interaction.reply({embeds: [
+                    new EmbedBuilder()
+                    .setColor('Blurple')
+                    .setFooter({ text: membro.username, iconURL: membro.displayAvatarURL({ dynamic: true })})
+                    .setDescription(`${data.Content.map((w, i) => 
+                            `**${emojis.ID} ${i + 1}:** \`${w.Data}\`\nAutor do aviso: **${w.ModeradorUsername} (${w.ModeradorID})**\nMotivo: **${w.Motivo}**\n\n`
+                    ).join(" ")}`)
+                ]})
+            } else {
+                interaction.reply({embeds: [
+                    new EmbedBuilder()
+                    .setColor('Blurple')
+                    .setFooter({ text: membro.username, iconURL: membro.displayAvatarURL({ dynamic: true })})
+                    .setDescription(`\`\`\`O usuário ${membro.username} (${membro.id}) não possui warns\`\`\``)
+                ]})
+            }
+        }).catch((err) => {
+            console.log(err);
+        })
+    }
+}
