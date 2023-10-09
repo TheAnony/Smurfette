045b6400affb96173e83d7ab1e574afcd61eed75
The Anony <theanonimooo14@gmail.com>
2023-10-09 10:57:38 -0300

    Comando base para configurar o canal modlogs
    Será alterado no futuro, pois dois comandos não podem ter o mesmo, isso inclui agrupamento de subcomandos

---
+++ Comandos/CONFIG/configCanalModLogs.js
@@ -0,0 +1,50 @@
+const { EmbedBuilder, ApplicationCommandType, ApplicationCommandOptionType, ChannelType } = require("discord.js")
+const emojis = require('../../emojis.json')
+const GuildConfig = require('../../Models/GuildConfig');
+
+module.exports = {
+    name: "configuracoes", // Coloque o nome do comando
+    description: "『 CONFIG 』.", // Coloque a descrição do comando
+    type: ApplicationCommandType.ChatInput,
+    options: [
+        {
+            name: 'modlogs',
+            description: `Configure as sugestões`,
+            type: ApplicationCommandOptionType.SubcommandGroup,
+            options: [
+                {
+                    name: 'adicionar',
+                    description: '『 CONFIG 』Adicione um canal de modlogs',
+                    type: ApplicationCommandOptionType.Subcommand,
+                    options: [
+                        {
+                            name: 'canal',
+                            description: 'Escolha qual canal será configurado.',
+                            type: ApplicationCommandOptionType.Channel,
+                            channelTypes: [ChannelType.GuildText],
+                            required: true
+                        }
+                    ]
+                },
+                {
+                    name: 'remover',
+                    description: '『 CONFIG 』Remova um canal de modlogs',
+                    type: ApplicationCommandOptionType.Subcommand,
+                    options: [
+                        {
+                            name: 'canal',
+                            description: 'Escolha qual canal será configurado.',
+                            type: ApplicationCommandOptionType.Channel,
+                            channelTypes: [ChannelType.GuildText],
+                            required: true
+                        }
+                    ]
+                }
+            ]
+        },
+    ],
+  
+    run: async (client, interaction) => {
+  
+    }
+  }
