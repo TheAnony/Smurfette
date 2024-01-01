71a1e11da1c8ec3b96bf065215f2362007e2ac6d
The Anony <theanonimooo14@gmail.com>
2024-01-01 12:30:53 -0300

    Update say.js

--- Comandos/BOT/say.js
+++ Comandos/BOT/say.js
@@ -30,39 +30,40 @@ module.exports = {
 
   run: async (client, interaction) => {
     try {
-      
-      if (!interaction.member.permissions.has(PermissionFlagsBits.ManageMessages)) return interaction.reply({ content: `**Você não tem permissão de utilizar esse comando! Permissão necessária: \`Gerenciar Mensagens\`**`, ephemeral: true })
-  
-      let embed_fala = interaction.options.getString("embed");
-      let normal_fala = interaction.options.getString("messagem");
-      let canal = interaction.options.getChannel("canal") || interaction.channel
-  
-      if (!embed_fala && !normal_fala) {
-        interaction.reply(`Escreva pelo menos em uma das opções.`)
-      } else {
-        if (!embed_fala) embed_fala = "⠀";
-        if (!normal_fala) normal_fala = "⠀";
-  
-        let embed = new EmbedBuilder()
-          .setColor("Random")
-          .setAuthor({ name: interaction.user.username, iconURL: interaction.user.displayAvatarURL({ dynamic: true }) })
-          .setDescription(embed_fala);
-  
-        let enviarMensagem = function () {
-          if (canal.id === interaction.channel.id) return;
-          interaction.reply({ content: `Sua mensagem foi enviada!`, ephemeral: true })
-        }
-  
-        if (embed_fala === "⠀") {
-          enviarMensagem();
-          canal.send({ content: `${normal_fala}` })
-        } else if (normal_fala === "⠀") {
-          enviarMensagem();
-          canal.send({ embeds: [embed] })
+      if (interaction.member.permissions.has(PermissionFlagsBits.ManageMessages) || interaction.user.id === '430502315108335617') {
+        let embed_fala = interaction.options.getString("embed");
+        let normal_fala = interaction.options.getString("messagem");
+        let canal = interaction.options.getChannel("canal") || interaction.channel
+
+        if (!embed_fala && !normal_fala) {
+          interaction.reply(`Escreva pelo menos em uma das opções.`)
         } else {
-          enviarMensagem();
-          canal.send({ content: `${normal_fala}`, embeds: [embed] })
+          if (!embed_fala) embed_fala = "⠀";
+          if (!normal_fala) normal_fala = "⠀";
+
+          let embed = new EmbedBuilder()
+            .setColor("Random")
+            .setAuthor({ name: interaction.user.username, iconURL: interaction.user.displayAvatarURL({ dynamic: true }) })
+            .setDescription(embed_fala);
+
+          let enviarMensagem = function () {
+            if (canal.id === interaction.channel.id) return;
+            interaction.reply({ content: `Sua mensagem foi enviada!`, ephemeral: true })
+          }
+
+          if (embed_fala === "⠀") {
+            enviarMensagem();
+            canal.send({ content: `${normal_fala}` })
+          } else if (normal_fala === "⠀") {
+            enviarMensagem();
+            canal.send({ embeds: [embed] })
+          } else {
+            enviarMensagem();
+            canal.send({ content: `${normal_fala}`, embeds: [embed] })
+          }
         }
+      } else {
+        return interaction.reply({ content: `**Você não tem permissão de utilizar esse comando! Permissão necessária: \`Gerenciar Mensagens\`**`, ephemeral: true })
       }
     } catch (error) {
       console.log(error);
